90b99292a1f5434eb0cd39222ff665e67c2d68fa
Persigio <sparkraven@gmail.com>
2021-05-26 15:10:14 -0400

    added icon prop
    from Nav to go to NavItem. Currently icon is not in use, will implement later

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import NavItem from '../navItem/navItem';
+
 class Nav extends React.Component {
     render(){
         return (
             <nav role='navigation'>
                 <ul>
-                    <NavItem destination='' title='About' />
-                    <NavItem destination='' title='Works' />
-                    <NavItem destination='' title='Contact' />
+                    <NavItem destination='' title='About' icon=''/>
+                    <NavItem destination='' title='Works' icon=''/>
+                    <NavItem destination='' title='Contact' icon=''/>
                 </ul>
             </nav>
         )
--- src/components/navItem/navItem.jsx
+++ src/components/navItem/navItem.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 class NavItem extends React.Component {
     render(){
         return (
