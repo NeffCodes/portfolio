44cfb6fdb33cf4fc183838265755e70adc325005
Persigio <sparkraven@gmail.com>
2021-05-28 14:16:47 -0400

    Added icons to nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -5,9 +5,9 @@ const Nav = () => {
     return (
         <nav role='navigation'>
             <ul>
-                <NavItem destination='#About' title='About' icon=''/>
-                <NavItem destination='#' title='Works' icon=''/>
-                <NavItem destination='#Contact' title='Contact' icon=''/>
+                <NavItem destination='#About' title='About' icon='far fa-user'/>
+                <NavItem destination='#' title='Works' icon='fas fa-desktop'/>
+                <NavItem destination='#Contact' title='Contact' icon='far fa-paper-plane'/>
             </ul>
         </nav>
     )
--- src/components/nav/navItem.jsx
+++ src/components/nav/navItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export const NavItem = ({title, destination}) => {
+export const NavItem = ({title, destination, icon}) => {
     return (
         <li>
+            
             <a href={destination}>
+                <i className={`${icon} fa-sm`}/>
                 {title}
             </a>
         </li>
