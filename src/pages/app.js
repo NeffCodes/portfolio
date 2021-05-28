40678bb959a65476f0d95d384c1034dcb82fae66
Persigio <sparkraven@gmail.com>
2021-05-28 14:17:31 -0400

    Update app.js

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,6 +3,7 @@ import Nav from '../components/nav/nav';
 import Hero from '../components/hero/hero';
 import About from '../components/about/about';
 import Contact from '../components/contact/contact';
+import Footer from '../components/footer/footer';
 
 const App = () => {
     return(
@@ -11,6 +12,7 @@ const App = () => {
             <Hero />
             <About />
             <Contact />
+            <Footer />
         </main>
     )
 }
