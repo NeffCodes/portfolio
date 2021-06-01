6d95a8ffc293a03d63a89bf1590d4df5f6336d6e
Persigio <sparkraven@gmail.com>
2021-06-01 14:16:55 -0400

    Update footerIcons.jsx
    - rearranged icons
    - added aria code based on awesome font docs
    - removed redundant if statement

--- src/components/footer/footerIcons.jsx
+++ src/components/footer/footerIcons.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 
 export const SocialIcons = () => {
+    
     const socials = [
-        {name: 'codepen', icon: 'codepen', destination: 'https://codepen.io/Persigio'},
-        {name: 'github', icon:'github', destination: 'https://github.com/persigio'},
-        {hame: 'linkedin', icon: 'linkedin', destination: 'https://www.linkedin.com/in/james-m-neff/'}
+        {name: 'CodePen', icon: 'codepen', destination: 'https://codepen.io/Persigio'},
+        {name: 'GitHub', icon:'github', destination: 'https://github.com/persigio'},
+        {hame: 'LinkedIn', icon: 'linkedin', destination: 'https://www.linkedin.com/in/james-m-neff/'}
     ];
+
     return (
         <div className='social-icons'>
         {
             socials.map( (social) => {
-                if(social){
-                    return(
-                        <a
-                            key={social.name}
-                            href={social.destination}
-                            rel='noopener noreferrer'
-                            target='_blank'
-                            aria-label={social.name}
-                        >
-                            <i className={`fab fa-${social.icon} fa-2x`}/>
-                        </a>
-                    );
-                }
+                return(
+                    <a
+                        key={social.name}
+                        href={social.destination}
+                        rel='noopener noreferrer'
+                        target='_blank'
+                        aria-label={social.name}
+                    >
+                        <i 
+                        className={`fab fa-${social.icon} fa-2x`}
+                        aria-hidden='true'
+                        />
+                    </a>
+                );
             })
         }
         </div>
