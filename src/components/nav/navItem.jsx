d0212252614eaf8a92686aca9b8031b0622f9b3f
Persigio <sparkraven@gmail.com>
2021-06-01 13:20:27 -0400

    Update navItem.jsx
    - added aria role to anchor tags

--- src/components/nav/navItem.jsx
+++ src/components/nav/navItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const NavItem = ({title, destination, icon, hidden}) => {
     return (
         <li hidden={hidden}>
-            <a href={destination}>
+            <a href={destination} role="button">
                 <i className={`${icon} fa-sm`}/>
                 {title}
             </a>
