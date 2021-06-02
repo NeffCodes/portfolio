c1a38cd7ccbefd9de608be9346a6c2051de69481
Persigio <sparkraven@gmail.com>
2021-06-02 16:32:28 -0400

    Update nav.jsx
    - set to match about.jsx
    - TO DO: fix useEffect to re-render on screen resize

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -3,20 +3,20 @@ import {NavItem} from './navItem';
 
 const Nav = () => {
 
-    const [isMobile, setIsMobile] = useState(false);
+    const [isLargeScreen, setIsLargeScreen] = useState(false);
 
     useEffect(() => {
-        if (window.innerWidth > 768) {
-        setIsMobile(false);
+        if (window.innerWidth < 768) {
+            setIsLargeScreen(false);
         } else {
-        setIsMobile(true);
+            setIsLargeScreen(true);
         }
     }, []);
 
     return (
         <nav role='navigation'>
             <ul>
-                <NavItem hidden={isMobile} destination='#' title='JNeff'/>                
+                {isLargeScreen && (<NavItem destination='#' title='JNeff'/>)}
                 <NavItem destination='#About' title='About' icon='far fa-user'/>
                 <NavItem destination='#' title='Works' icon='fas fa-desktop'/>
                 <NavItem destination='#Contact' title='Contact' icon='far fa-paper-plane'/>
