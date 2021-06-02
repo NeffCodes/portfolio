4f24cad75085c6e43c659df920979fa5140ef9f3
Persigio <sparkraven@gmail.com>
2021-06-02 14:12:36 -0400

    Update about.jsx
    - added portrait on large screens
    - moved paragraph, skilltags, and button into div

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button} from '../button/button';
 import {SkillTags} from './skillTags';
 
 const About = () => {
+
+    const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth < 768) {
+            setIsLargeScreen(false);
+        } else {
+            setIsLargeScreen(true);
+        }
+    }, []);
+
     return(
         <section className='about' id='About'>
             <h2>
                 About
             </h2>
-            <p>
-                Blah blah blah, I need to figure out my short elevator pitch to put here. Yadda yadaa, in short, please hire me, I would like money. 
-            </p>
-            
-            <SkillTags />
-            <Button classes='btn primary' content='See My Work' />
+            <div>
+                {isLargeScreen && (
+                    <div className='portrait' aria-hidden='true'>
+                        <div className='shape'></div>
+                    </div>
+                )}
+                
+                <div className='description'>
+                    <p>
+                        Blah blah blah, I need to figure out my short elevator pitch to put here. Yadda yadaa, in short, please hire me, I would like money. 
+                    </p>
+                    <SkillTags />
+                    <Button classes='btn primary' content='See My Work' /> 
+                </div>
+            </div>    
         </section>
     )
 }
