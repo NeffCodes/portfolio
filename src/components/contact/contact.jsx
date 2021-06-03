5b6e55c253e967225770dc99932fd5f179490f38
Persigio <sparkraven@gmail.com>
2021-06-03 12:37:38 -0400

    Update contact.css
    - moved to module of contact

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { ExternalButton } from '../button/externalButton/externalButton';
+import { contact } from './contact.module.css';
 
 const Contact = () => {
     return(
-        <section className='contact' id='Contact'>
-            <h2>
-                Say Hello
-            </h2>
+        <section className={contact} id='Contact'>
+            <h2>Say Hello</h2>
             <p>Would you like to work with me or have a question?</p>
             <p>Don't be shy, let me know! </p>
             <ExternalButton content="Let's Talk" destination='mailto:test@gmail.com'/>
