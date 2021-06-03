a13862bcd2a03d72c22d5aa54036d73ec0966fb4
Persigio <sparkraven@gmail.com>
2021-06-03 12:27:29 -0400

    Create work.js

---
+++ src/pages/work.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const Works = () => {
+    return(
+        <main>
+            <title>My Works</title>
+            <h1>My works by me</h1>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </main>
+    )
+}
+
+export default Works
