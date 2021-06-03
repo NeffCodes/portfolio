6f9c0ce7e370b401055a4f531f3ef43f552a29fe
Persigio <sparkraven@gmail.com>
2021-06-03 12:46:15 -0400

    Update contact.jsx
    - playing with wording

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,10 +5,10 @@ import { contact } from './contact.module.css';
 const Contact = () => {
     return(
         <section className={contact} id='Contact'>
-            <h2>Say Hello</h2>
-            <p>Would you like to work with me or have a question?</p>
+            <h2>Give a Shout</h2>
+            <p>Have a question or just want to chat?</p>
             <p>Don't be shy, let me know! </p>
-            <ExternalButton content="Let's Talk" destination='mailto:test@gmail.com'/>
+            <ExternalButton content="Say Hello" destination='mailto:test@gmail.com'/>
         </section>
     )
 }
