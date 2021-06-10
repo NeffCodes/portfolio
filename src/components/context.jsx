0f0c9c5d2613f4315e28c2de4086529af9d0c334
Persigio <sparkraven@gmail.com>
2021-06-10 16:03:21 -0400

    Update context.jsx
    - typo fixed

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -11,5 +11,5 @@ export const skills = [
 export const socials = [
         {name: 'CodePen', icon: 'codepen', destination: 'https://codepen.io/Persigio', key: 'social-codepen'},
         {name: 'GitHub', icon:'github', destination: 'https://github.com/persigio', key: 'social-github'},
-        {hame: 'LinkedIn', icon: 'linkedin', destination: 'https://www.linkedin.com/in/james-m-neff/', key: 'social-linkedin'} 
+        {name: 'LinkedIn', icon: 'linkedin', destination: 'https://www.linkedin.com/in/james-m-neff/', key: 'social-linkedin'} 
     ];
