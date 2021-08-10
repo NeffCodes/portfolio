2ca8fd8382936202bd5cc6026722f0f0d6b064dc
Persigio <sparkraven@gmail.com>
2021-08-10 15:50:40 -0400

    Update context.jsx

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -1,10 +1,9 @@
 export const userName = 'James Neff';
 
 export const aboutDescription = `
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-Excepturi neque, ipsa animi maiores repellendu distinctioaperiam 
-earum dolor voluptatum consequatur blanditiis inventore debitis fuga
-numquam voluptate architecto itaque molestiae.
+With over 9 years working across different areas of print & digital design, I am able to provide a creative insight to your next project. 
+These days, I have been spending my time researching, designing and coding, although you may catch me learning a new board game with friends. 
+Currently, I'm looking for the next challenge to apply my skills to, so if you're interested let me know!
 `;
 
 export const skills = [
@@ -12,13 +11,13 @@ export const skills = [
     {name:'HTML5', icon:'html5', key:'skill-html'},
     {name:'Javascript', icon:'js-square', key:'skill-javascript'},
     {name:'React-Redux', icon:'react', key:'skill-react'},
+    {name:'Bootstrap', icon: 'bootstrap', key:'skill-bootstrap'},
     {name:'git', icon:'git', key:'skill-git'},
     {name:'Figma', icon:'figma', key:'skill-figma'},
-    {name:'Node', icon:'node', key:'skill-node'}
-    ];
+];
 
 export const socials = [
-        {name: 'CodePen', icon: 'codepen', destination: 'https://codepen.io/Persigio', key: 'social-codepen'},
-        {name: 'GitHub', icon:'github', destination: 'https://github.com/persigio', key: 'social-github'},
-        {name: 'LinkedIn', icon: 'linkedin', destination: 'https://www.linkedin.com/in/james-m-neff/', key: 'social-linkedin'} 
-    ];
+    {name: 'CodePen', icon: 'codepen', destination: 'https://codepen.io/Persigio', key: 'social-codepen'},
+    {name: 'GitHub', icon:'github', destination: 'https://github.com/persigio', key: 'social-github'},
+    {name: 'LinkedIn', icon: 'linkedin', destination: 'https://www.linkedin.com/in/james-m-neff/', key: 'social-linkedin'} 
+];
