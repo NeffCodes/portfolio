8af890b9cedec9c7625892bf9f68715a0e7695a0
Persigio <sparkraven@gmail.com>
2021-08-13 16:44:22 -0400

    Fix window bug with gatsby ssr

--- src/components/layout/nav/nav.jsx
+++ src/components/layout/nav/nav.jsx
@@ -4,11 +4,30 @@ import useWindowSize from '../../../utils/useWindowSize';
 
 const Nav = () => {
     const { width } = useWindowSize();
+    const [w, setW] = React.useState(width)
+        
+    //useEffect courtesy of Vitalie Maldur from dev.to
+    React.useEffect(() => {
+        const resizeListener = () => {
+            setW(width)
+        }
+        //set resize listener
+        if(typeof window !== 'undefined'){
+            window.addEventListener('resize',resizeListener);
+        }
+        
+        //clean up
+        return () => {
+            if(typeof window !== 'undefined'){
+                window.removeEventListener('resize', resizeListener);
+            }
+        }
+    }, [width]);
 
     return (
         <nav role='navigation' className='nav'>
             <ul className='nav-container'>
-                {width > 1023 && (<li className='logo'>JNeff</li>)}
+                {w > 1023 && (<li className='logo'>JNeff</li>)}
                 <NavItem destination='/#About' title='About' icon='far fa-user'/>
                 <NavItem destination='/works' title='Works' icon='fas fa-desktop'/>
                 <NavItem destination='/#Contact' title='Contact' icon='far fa-paper-plane'/>
