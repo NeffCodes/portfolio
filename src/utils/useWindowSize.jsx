cb080ea68fe1df20a6cc64463e6fa9b11cd4c571
Persigio <sparkraven@gmail.com>
2021-08-13 16:52:56 -0400

    Update useWindowSize.jsx

--- src/utils/useWindowSize.jsx
+++ src/utils/useWindowSize.jsx
@@ -1,16 +1,24 @@
 export default function useWindowSize() {
   if (typeof window !== 'undefined') {
-    return (
-      {
+    if(window.innerWidth){
+      return {
         width: window.innerWidth,
         height: window.innerHeight,
-      } || {
+      }
+    }
+
+    if (document.documentElement.clientWidth) {
+      return {
         width: document.documentElement.clientWidth,
         height: document.documentElement.clientHeight,
-      } || {
+      }
+    }
+
+    if (document.body.clientWidth) {
+      return {
         width: document.body.clientWidth,
         height: document.body.clientHeight,
       }
-    );
+    }
   }
 }
