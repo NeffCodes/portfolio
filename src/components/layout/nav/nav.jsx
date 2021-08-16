d94305548fedffe53574c4e9314b3a72294423b7
Persigio <sparkraven@gmail.com>
2021-08-16 16:55:55 -0400

    Remove logo

--- src/components/layout/nav/nav.jsx
+++ src/components/layout/nav/nav.jsx
@@ -1,32 +1,10 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import { NavItem } from './navItem';
-import { getWidth } from '../../../utils/useWindowSize';
 
 const Nav = () => {
-    const [width, setWidth] = useState(getWidth());
-    
-    //useEffect courtesy of Vitalie Maldur from dev.to
-    useEffect(() => {
-        const resizeListener = () => {
-            setWidth(getWidth())
-        }
-        //set resize listener
-        if(typeof window !== 'undefined'){
-            window.addEventListener('resize',resizeListener);
-        }
-        
-        //clean up
-        return () => {
-            if(typeof window !== 'undefined'){
-                window.removeEventListener('resize', resizeListener);
-            }
-        }
-    }, []);
-
     return (
         <nav role='navigation' className='nav'>
             <ul className='nav-container'>
-                {width > 1023 && (<li className='logo'>JNeff</li>)}
                 <NavItem destination='/#About' title='About' icon='far fa-user'/>
                 <NavItem destination='/works' title='Works' icon='fas fa-desktop'/>
                 <NavItem destination='/#Contact' title='Contact' icon='far fa-paper-plane'/>
